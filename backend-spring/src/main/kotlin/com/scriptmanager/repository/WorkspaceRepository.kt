84967afad92de25699ee6e61bc476aa106f0cd3f
machingclee <machingclee@gmail.com>
2026-01-05 12:47:15 +0800

    add left join fetch

--- backend-spring/src/main/kotlin/com/scriptmanager/repository/WorkspaceRepository.kt
+++ backend-spring/src/main/kotlin/com/scriptmanager/repository/WorkspaceRepository.kt
@@ -2,8 +2,19 @@ package com.scriptmanager.repository
 
 import com.scriptmanager.common.entity.Workspace
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 
 @Repository
-interface WorkspaceRepository : JpaRepository<Workspace, Int>
+interface WorkspaceRepository : JpaRepository<Workspace, Int> {
+    @Query(
+        """
+        select ws from Workspace ws
+        left join fetch ws.folders folder
+        left join fetch folder.shellScripts scripts
+        left join fetch folder.parentFolder
+    """
+    )
+    fun findAllFetchingFoldersAndShellScripts(): List<Workspace>
+}
 
